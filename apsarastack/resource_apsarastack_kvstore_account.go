770c348686a9fb781df2796bd51883c2e3b4ea51
chenchi <cc01032862@alibaba-inc.com>
2022-09-08 11:45:01 +0800

    Add DRDS Authorize

--- apsarastack/resource_apsarastack_kvstore_account.go
+++ apsarastack/resource_apsarastack_kvstore_account.go
@@ -83,7 +83,7 @@ func resourceApsaraStackKVStoreAccountCreate(d *schema.ResourceData, meta interf
 	request.QueryParams = map[string]string{"AccessKeySecret": client.SecretKey, "Product": "R-kvstore", "Department": client.Department, "ResourceGroup": client.ResourceGroup}
 	request.InstanceId = d.Get("instance_id").(string)
 	request.AccountName = d.Get("account_name").(string)
-
+	request.AccountPrivilege = d.Get("account_privilege").(string)
 	password := d.Get("account_password").(string)
 	kmsPassword := d.Get("kms_encrypted_password").(string)
 
