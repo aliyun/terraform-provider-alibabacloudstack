7402e93574b9aa4ab33155824ddb0e546eef7980
刘栋 <ld01016998@alibaba-inc.com>
2024-12-12 13:12:25 +0800

    slb acl

--- alibabacloudstack/resource_apsarastack_slb_accesscontrollist_test.go
+++ alibabacloudstack/resource_apsarastack_slb_accesscontrollist_test.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/slb"
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/connectivity"
-	
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccAlibabacloudStackSlbAccesscontrollist0(t *testing.T) {
-	var v map[string]interface{}
+	var v *slb.DescribeAccessControlListAttributeResponse
 
 	resourceId := "alibabacloudstack_slb_accesscontrollist.default"
 	ra := resourceAttrInit(resourceId, AlibabacloudTestAccSlbAccesscontrollistCheckmap)
@@ -22,7 +23,11 @@ func TestAccAlibabacloudStackSlbAccesscontrollist0(t *testing.T) {
 
 	rand := getAccTestRandInt(10000, 99999)
 	name := fmt.Sprintf("tf-testacc%sslbaccess_control_list%d", defaultRegionToTest, rand)
-
+	entry := make([]map[string]string, 0)
+	entry = append(entry, map[string]string{
+		"entry":   "192.168.1.0/24",
+		"comment": "test_entry",
+	})
 	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlibabacloudTestAccSlbAccesscontrollistBasicdependence)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -38,19 +43,14 @@ func TestAccAlibabacloudStackSlbAccesscontrollist0(t *testing.T) {
 
 			{
 				Config: testAccConfig(map[string]interface{}{
+					"acl_name": "Rdk_test_name01",
 
 					"address_ip_version": "ipv4",
-
-					"resource_group_id": "${{ref(resource, ResourceManager::ResourceGroup::3.0.0::defaultsRG.ResourceGroupId)}}",
-
-					"acl_name": "Rdk_test_name01",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
 
 						"address_ip_version": "ipv4",
-
-						"resource_group_id": CHECKSET,
 
 						"acl_name": "Rdk_test_name01",
 					}),
@@ -60,75 +60,88 @@ func TestAccAlibabacloudStackSlbAccesscontrollist0(t *testing.T) {
 			{
 				Config: testAccConfig(map[string]interface{}{
 
-					"acl_name": "Rdk-test-name",
+					"address_ip_version": "ipv6",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
 
-						"acl_name": "Rdk-test-name",
+						"address_ip_version": "ipv6",
 					}),
 				),
 			},
 
-			{
-				Config: testAccConfig(map[string]interface{}{
-					"tags": map[string]string{
-						"Created": "TF",
-						"For":     "Test",
-					},
-				}),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(map[string]string{
-						"tags.%":       "2",
-						"tags.Created": "TF",
-						"tags.For":     "Test",
-					}),
-				),
-			},
-			{
-				Config: testAccConfig(map[string]interface{}{
-					"tags": map[string]string{
-						"Created": "TF-update",
-						"For":     "Test-update",
-					},
-				}),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(map[string]string{
-						"tags.%":       "2",
-						"tags.Created": "TF-update",
-						"tags.For":     "Test-update",
-					}),
-				),
-			},
-			{
-				Config: testAccConfig(map[string]interface{}{
-					"tags": REMOVEKEY,
-				}),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(map[string]string{
-						"tags.%":       "0",
-						"tags.Created": REMOVEKEY,
-						"tags.For":     REMOVEKEY,
-					}),
-				),
-			},
+			// {
+			// 	Config: testAccConfig(map[string]interface{}{
+
+			// 		"entry_list": entry,
+			// 	}),
+			// 	Check: resource.ComposeTestCheckFunc(
+			// 		testAccCheck(map[string]string{
+
+			// 			"entry_list.%": "1",
+			// 		}),
+			// 	),
+			// },
+
+			// 	{
+			// 		Config: testAccConfig(map[string]interface{}{
+			// 			"tags": map[string]string{
+			// 				"Created": "TF1",
+			// 				"For":     "Test1",
+			// 			},
+			// 		}),
+			// 		Check: resource.ComposeTestCheckFunc(
+			// 			testAccCheck(map[string]string{
+			// 				"tags.%":       "2",
+			// 				"tags.Created": "TF1",
+			// 				"tags.For":     "Test1",
+			// 			}),
+			// 		),
+			// 	},
+			// 	{
+			// 		Config: testAccConfig(map[string]interface{}{
+			// 			"tags": map[string]string{
+			// 				"Created": "TF-update",
+			// 				"For":     "Test-update",
+			// 			},
+			// 		}),
+			// 		Check: resource.ComposeTestCheckFunc(
+			// 			testAccCheck(map[string]string{
+			// 				"tags.%":       "2",
+			// 				"tags.Created": "TF-update",
+			// 				"tags.For":     "Test-update",
+			// 			}),
+			// 		),
+			// 	},
+			// 	{
+			// 		Config: testAccConfig(map[string]interface{}{
+			// 			"tags": REMOVEKEY,
+			// 		}),
+			// 		Check: resource.ComposeTestCheckFunc(
+			// 			testAccCheck(map[string]string{
+			// 				"tags.%":       "0",
+			// 				"tags.Created": REMOVEKEY,
+			// 				"tags.For":     REMOVEKEY,
+			// 			}),
+			// 		),
+			// 	},
 		},
 	})
 }
 
 var AlibabacloudTestAccSlbAccesscontrollistCheckmap = map[string]string{
 
-	"address_ip_version": CHECKSET,
+	// "address_ip_version": CHECKSET,
 
-	"resource_group_id": CHECKSET,
+	// "resource_group_id": CHECKSET,
 
-	"acl_id": CHECKSET,
+	// "acl_id": CHECKSET,
 
-	"related_listeners": CHECKSET,
+	// "related_listeners": CHECKSET,
 
-	"acl_name": CHECKSET,
+	// "acl_name": CHECKSET,
 
-	"tags": CHECKSET,
+	// "tags": CHECKSET,
 }
 
 func AlibabacloudTestAccSlbAccesscontrollistBasicdependence(name string) string {
