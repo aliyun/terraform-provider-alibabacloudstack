d40e3d6892560c166cf9e053b3be6774bbb9216e
dwt01285077 <dwt01285077@alibaba-inc.com>
2024-12-12 15:41:48 +0800

    fix debugs

--- alibabacloudstack/connectivity/client.go
+++ alibabacloudstack/connectivity/client.go
@@ -1231,7 +1231,6 @@ func (client *AlibabacloudStackClient) DoTeaRequest(method string, popcode strin
 	sdkConfig := client.teaSdkConfig
 	sdkConfig.SetEndpoint(endpoint).SetReadTimeout(client.Config.ClientReadTimeout * 1000) //单位毫秒
 	conn, err := rpc.NewClient(&sdkConfig)
-<<<<<<< HEAD
 	conn.Headers = make(map[string]*string)
 	for key, value := range client.defaultHeaders(popcode) {
 		conn.Headers[key] = &value
@@ -1242,8 +1241,6 @@ func (client *AlibabacloudStackClient) DoTeaRequest(method string, popcode strin
 	}
 	query["Product"] = popcode
 
-=======
->>>>>>> ff546a6f94d1b8341d8908267f17fd6f2669aca3
 	if err != nil {
 		return nil, fmt.Errorf("unable to initialize the %s client: %#v", popcode, err)
 	}
