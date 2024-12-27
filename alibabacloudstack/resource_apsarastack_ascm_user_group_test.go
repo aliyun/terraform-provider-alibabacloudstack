b84e4a8fa0a1c8a80335b8125da52b08c239b56e
儆钰 <jingyu.wy@alibaba-inc.com>
2024-12-27 17:10:27 +0800

    ascm_user_group的organization_id属性改为Computed，测试时不在需要环境变量

--- alibabacloudstack/resource_apsarastack_ascm_user_group_test.go
+++ alibabacloudstack/resource_apsarastack_ascm_user_group_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/connectivity"
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/errmsgs"
 
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -15,7 +14,6 @@ import (
 
 func TestAccAlibabacloudStackAscm_User_Group_Basic(t *testing.T) {
 	var v *UserGroup
-	var org_id string
 	resourceId := "alibabacloudstack_ascm_user_group.default"
 	ra := resourceAttrInit(resourceId, ascmusergroupBasicMap)
 	serviceFunc := func() interface{} {
@@ -26,11 +24,6 @@ func TestAccAlibabacloudStackAscm_User_Group_Basic(t *testing.T) {
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	rand := getAccTestRandInt(10000, 20000)
 	name := fmt.Sprintf("tf-ascmusergroup%v", rand)
-	if os.Getenv("ALIBABACLOUDSTACK_DEPARTMENT") != "" {
-		org_id = os.Getenv("ALIBABACLOUDSTACK_DEPARTMENT")
-	} else {
-		org_id = "${alibabacloudstack_ascm_organization.default.org_id}"
-	}
 	testAccConfig := resourceTestAccConfigFunc(resourceId, name, testascmusergroupconfigbasic)
 	ResourceTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -45,7 +38,6 @@ func TestAccAlibabacloudStackAscm_User_Group_Basic(t *testing.T) {
 			{
 				Config: testAccConfig(map[string]interface{}{
 					"group_name":      name,
-					"organization_id": org_id,
 					"role_in_ids":     []string{"2", "6"},
 				}),
 				Check: resource.ComposeTestCheckFunc(
@@ -77,26 +69,11 @@ func testAccCheckAscm_User_Group_Destroy(s *terraform.State) error { //destroy f
 	return nil
 }
 func testascmusergroupconfigbasic(name string) string {
-	if os.Getenv("ALIBABACLOUDSTACK_DEPARTMENT") != "" {
-		return fmt.Sprintf(`
+	return fmt.Sprintf(`
 variable name{
  default = "%s"
 }
 `, name)
-	} else {
-		return fmt.Sprintf(`
-variable name{
- default = "%s"
-}
-
-
-// resource "alibabacloudstack_ascm_organization" "default" {
-//  name = "terraform"
-//  parent_id = "1"
-// }
-
-`, name)
-	}
 }
 
 var ascmusergroupBasicMap = map[string]string{
