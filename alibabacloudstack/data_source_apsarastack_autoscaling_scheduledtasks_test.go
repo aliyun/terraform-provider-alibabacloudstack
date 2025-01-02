b05a5fe270e0a4060e3de3010248e7272f68f07a
刘栋 <ld01016998@alibaba-inc.com>
2025-01-02 16:44:41 +0800

    ess_scheduledtask test

--- alibabacloudstack/data_source_apsarastack_autoscaling_scheduledtasks_test.go
+++ alibabacloudstack/data_source_apsarastack_autoscaling_scheduledtasks_test.go
@@ -2,9 +2,10 @@ package alibabacloudstack
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"testing"
 	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccAlibabacloudStackEssScheduledtasksDataSource(t *testing.T) {
@@ -16,7 +17,7 @@ func TestAccAlibabacloudStackEssScheduledtasksDataSource(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(testAccCheckAlibabacloudStackEssScheduledTasksDataSource, time.Now().Add(oneDay).Format("2006-01-02T15:04Z")),
+				Config: fmt.Sprintf(testAccCheckAlibabacloudStackEssScheduledTasksDataSource, ECSInstanceCommonTestCase, time.Now().Add(oneDay).Format("2006-01-02T15:04Z")),
 				Check: resource.ComposeTestCheckFunc(
 
 					testAccCheckAlibabacloudStackDataSourceID("data.alibabacloudstack_ess_scheduled_tasks.default"),
@@ -35,36 +36,39 @@ variable "name" {
 	default = "tf-testAccDataSourceScheduledtas"
 }
 
-data "alibabacloudstack_zones" "default" {
-  available_resource_creation = "VSwitch"
-}
-
-resource "alibabacloudstack_vpc" "default" {
-  name = "${var.name}"
-  cidr_block = "10.0.0.0/8"
-}
-
-resource "alibabacloudstack_vswitch" "default" {
-  vpc_id = "${alibabacloudstack_vpc.default.id}"
-  cidr_block = "10.1.0.0/16"
-  availability_zone = "${data.alibabacloudstack_zones.default.zones.0.id}"
-  name = "${var.name}"
-}
-
+%s
 resource "alibabacloudstack_ess_scaling_group" "default" {
 	min_size = 0
 	max_size = 2
 	default_cooldown = 20
 	removal_policies = ["OldestInstance", "NewestInstance"]
 	scaling_group_name = "${var.name}"
-	vswitch_ids = ["${alibabacloudstack_vswitch.default.id}"]
+	vswitch_ids = ["${alibabacloudstack_vpc_vswitch.default.id}"]
 }
 
+resource "alibabacloudstack_ecs_deployment_set" "default" {
+	strategy            = "Availability"
+	domain              = "Default"
+	granularity         = "Host"
+	deployment_set_name = "example_value"
+	description         = "example_value"
+}
+
+resource "alibabacloudstack_ess_scaling_configuration" "default" {
+	scaling_group_id = "${alibabacloudstack_ess_scaling_group.default.id}"
+	image_id = "${data.alibabacloudstack_images.default.images.0.id}"
+	instance_type = "ecs.e4.small"
+	security_group_ids = [alibabacloudstack_ecs_securitygroup.default.id]
+	force_delete = true
+	active = true
+	enable = true
+	deployment_set_id = alibabacloudstack_ecs_deployment_set.default.id
+}
 resource "alibabacloudstack_ess_scaling_rule" "default" {
-  scaling_group_id = "${alibabacloudstack_ess_scaling_group.default.id}"
-  adjustment_type  = "TotalCapacity"
-  adjustment_value = 2
-  cooldown         = 60
+	scaling_group_id = "${alibabacloudstack_ess_scaling_group.default.id}"
+	adjustment_type = "TotalCapacity"
+	adjustment_value = "1"
+	cooldown = 0
 }
 
 resource "alibabacloudstack_ess_scheduled_task" "default" {
