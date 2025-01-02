6a7a7e40068a3ae1079ce40cf6df8597db958313
儆钰 <jingyu.wy@alibaba-inc.com>
2025-01-02 18:59:58 +0800

    nas测试用例修复

--- alibabacloudstack/data_source_apsarastack_nas_mounttargets_test.go
+++ alibabacloudstack/data_source_apsarastack_nas_mounttargets_test.go
@@ -56,11 +56,11 @@ func TestAccAlibabacloudStackNasMountTargetDataSource(t *testing.T) {
 	vpcIdConf := dataSourceTestAccConfig{
 		existConfig: testAccCheckAlibabacloudStackMountTargetDataSourceConfig(rand, map[string]string{
 			"file_system_id": `"${alibabacloudstack_nas_mount_target.default.file_system_id}"`,
-			"vpc_id":         `"${alibabacloudstack_vpc.default.id}"`,
+			"vpc_id":         `"${alibabacloudstack_vpc_vpc.default.id}"`,
 		}),
 		fakeConfig: testAccCheckAlibabacloudStackMountTargetDataSourceConfig(rand, map[string]string{
 			"file_system_id": `"${alibabacloudstack_nas_mount_target.default.file_system_id}"`,
-			"vpc_id":         `"${alibabacloudstack_vpc.default.id}"`,
+			"vpc_id":         `"${alibabacloudstack_vpc_vpc.default.id}"`,
 		}),
 	}
 	vswitchIdConf := dataSourceTestAccConfig{
@@ -100,7 +100,7 @@ func TestAccAlibabacloudStackNasMountTargetDataSource(t *testing.T) {
 			"vswitch_id":          `"${alibabacloudstack_nas_mount_target.default.vswitch_id}"`,
 			"type":                `"${alibabacloudstack_nas_access_group.default.access_group_type}"`,
 			"network_type":        `"${alibabacloudstack_nas_access_group.default.access_group_type}"`,
-			"vpc_id":              `"${alibabacloudstack_vpc.default.id}"`,
+			"vpc_id":              `"${alibabacloudstack_vpc_vpc.default.id}"`,
 			"mount_target_domain": `split(":",alibabacloudstack_nas_mount_target.default.id)[1]`,
 			"status":              `"Active"`,
 		}),
@@ -110,7 +110,7 @@ func TestAccAlibabacloudStackNasMountTargetDataSource(t *testing.T) {
 			"vswitch_id":          `"${alibabacloudstack_nas_mount_target.default.vswitch_id}_fake"`,
 			"type":                `"${alibabacloudstack_nas_access_group.default.access_group_type}_fake"`,
 			"network_type":        `"${alibabacloudstack_nas_access_group.default.access_group_type}_fake}"`,
-			"vpc_id":              `"${alibabacloudstack_vpc.default.id}"`,
+			"vpc_id":              `"${alibabacloudstack_vpc_vpc.default.id}"`,
 			"mount_target_domain": `"fake"`,
 			"status":              `"Inactive"`,
 		}),
@@ -125,53 +125,36 @@ func testAccCheckAlibabacloudStackMountTargetDataSourceConfig(rand int, attrMap 
 	}
 	config := fmt.Sprintf(`
 variable "name" {
-			default = "tf-testAccVswitch"
-}
-variable "description" {
-  default = "tf-testAccCheckAlibabacloudStackFileSystemsDataSource"
-}
-//data "alibabacloudstack_vpcs" "default" {
-//			name_regex = "lz_vpc3"
-//}
-
-resource "alibabacloudstack_vpc" "default" {
-  cidr_block = "172.16.0.0/16"
-  name = "${var.name}"
-}
-data "alibabacloudstack_zones" "default" {
-	available_resource_creation = "VSwitch"
+			default = "tf-testAccCheck-nasmount%d"
 }
 
-resource "alibabacloudstack_vswitch" "default" {
-  vpc_id = "${alibabacloudstack_vpc.default.id}"
-  cidr_block = "172.16.0.0/21"
-  availability_zone = "${data.alibabacloudstack_zones.default.zones.0.id}"
-  name = "${var.name}"
-}
 variable "storage_type" {
   default = "Capacity"
 }
+
+%s
+
 data "alibabacloudstack_nas_protocols" "default" {
         type = "${var.storage_type}"
 }
 resource "alibabacloudstack_nas_file_system" "default" {
-  description = "${var.description}"
+  description = "${var.name}"
   storage_type = "${var.storage_type}"
   protocol_type = "${data.alibabacloudstack_nas_protocols.default.protocols.0}"
 }
 resource "alibabacloudstack_nas_access_group" "default" {
-			access_group_name = "tf-testAccNasConfig-%d"
+			access_group_name = "${var.name}"
 			access_group_type = "Vpc"
-			description = "tf-testAccNasConfig"
+			description = "${var.name}"
 }
 resource "alibabacloudstack_nas_mount_target" "default" {
 			file_system_id = "${alibabacloudstack_nas_file_system.default.id}"
 			access_group_name = "${alibabacloudstack_nas_access_group.default.access_group_name}"
-			vswitch_id = "${alibabacloudstack_vswitch.default.id}"
+			vswitch_id = "${alibabacloudstack_vpc_vswitch.default.id}"
 }
 data "alibabacloudstack_nas_mount_targets" "default" {
 		%s
-}`, rand, strings.Join(pairs, "\n  "))
+}`, rand, VSwitchCommonTestCase, strings.Join(pairs, "\n  "))
 	return config
 }
 
