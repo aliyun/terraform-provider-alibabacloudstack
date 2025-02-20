bf32619221e72f4c1e1077ee875c7ae7953c411e
儆钰 <jingyu.wy@alibaba-inc.com>
2025-02-20 17:00:13 +0800

    fix func name

--- alibabacloudstack/resource_apsarastack_apigateway_app_test.go
+++ alibabacloudstack/resource_apsarastack_apigateway_app_test.go
@@ -2,19 +2,87 @@ package alibabacloudstack
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"testing"
+	"time"
+
+	
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/cloudapi"
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/connectivity"
-	
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccAlibabacloudStackApigatewayAppAttachment(t *testing.T) {
-	var v *cloudapi.AuthorizedApp
+func init() {
+	resource.AddTestSweepers("alibabacloudstack_api_gateway_app", &resource.Sweeper{
+		Name: "alibabacloudstack_api_gateway_app",
+		F:    testSweepApiGatewayApp,
+	})
+}
 
-	resourceId := "alibabacloudstack_api_gateway_app_attachment.default"
-	ra := resourceAttrInit(resourceId, apigatewayAppAttachmentBasicMap)
+func testSweepApiGatewayApp(region string) error {
+	rawClient, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("error getting AlibabacloudStack client: %s", err)
+	}
+	client := rawClient.(*connectivity.AlibabacloudStackClient)
+
+	prefixes := []string{
+		"tf-testacc",
+		"tf_testacc",
+	}
+
+	req := cloudapi.CreateDescribeAppAttributesRequest()
+	raw, err := client.WithCloudApiClient(func(cloudApiClient *cloudapi.Client) (interface{}, error) {
+		return cloudApiClient.DescribeAppAttributes(req)
+	})
+	if err != nil {
+		return fmt.Errorf("Error Describe Apps: %s", err)
+	}
+	apps, _ := raw.(*cloudapi.DescribeAppAttributesResponse)
+
+	swept := false
+
+	for _, v := range apps.Apps.AppAttribute {
+		name := v.AppName
+		skip := true
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(strings.ToLower(name), strings.ToLower(prefix)) {
+				skip = false
+				break
+			}
+		}
+		if skip {
+			log.Printf("[INFO] Skipping app: %s", name)
+			continue
+		}
+		swept = true
+
+		log.Printf("[INFO] Deleting App: %s", name)
+
+		req := cloudapi.CreateDeleteAppRequest()
+		req.AppId = requests.NewInteger64(v.AppId)
+		_, err := client.WithCloudApiClient(func(cloudApiClient *cloudapi.Client) (interface{}, error) {
+			return cloudApiClient.DeleteApp(req)
+		})
+		if err != nil {
+			log.Printf("[ERROR] Failed to delete App (%s): %s", name, err)
+		}
+	}
+	if swept {
+		time.Sleep(5 * time.Second)
+	}
+	return nil
+}
+
+func SkipTestAccAlibabacloudStackApigatewayApp_basic(t *testing.T) {
+	var v *cloudapi.DescribeAppResponse
+
+	resourceId := "alibabacloudstack_api_gateway_app.default"
+	ra := resourceAttrInit(resourceId, apigatewayAppBasicMap)
 
 	serviceFunc := func() interface{} {
 		return &CloudApiService{testAccProvider.Meta().(*connectivity.AlibabacloudStackClient)}
@@ -26,7 +94,7 @@ func TestAccAlibabacloudStackApigatewayAppAttachment(t *testing.T) {
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	rand := getAccTestRandInt(1000000, 9999999)
 	name := fmt.Sprintf("tf_testAccApp_%d", rand)
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceApigatewayAppAttachmentConfigDependence)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceApigatewayAppConfigDependence)
 
 	ResourceTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -39,72 +107,135 @@ func TestAccAlibabacloudStackApigatewayAppAttachment(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"api_id":     "${alibabacloudstack_api_gateway_api.default.api_id}",
-					"group_id":   "${alibabacloudstack_api_gateway_group.default.id}",
-					"stage_name": "PRE",
-					"app_id":     "${alibabacloudstack_api_gateway_app.default.id}",
+					"name": "${var.name}",
 				}),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(nil),
+					testAccCheck(map[string]string{
+						"name": name,
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description": "${var.description}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description": "tf_testAcc api gateway description",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name": "${var.name}_u",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"name": name + "_u",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description": "${var.description}_u",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description": "tf_testAcc api gateway description_u",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF",
+						"For":     "acceptance test",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF",
+						"tags.For":     "acceptance test",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name":        "${var.name}",
+					"description": "${var.description}",
+					"tags":        REMOVEKEY,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"name":         name,
+						"description":  "tf_testAcc api gateway description",
+						"tags.%":       REMOVEKEY,
+						"tags.Created": REMOVEKEY,
+						"tags.For":     REMOVEKEY,
+					}),
 				),
 			},
 		},
 	})
 }
 
-func resourceApigatewayAppAttachmentConfigDependence(name string) string {
+func SkipTestAccAlibabacloudStackApigatewayApp_multi(t *testing.T) {
+	var v *cloudapi.DescribeAppResponse
+	resourceId := "alibabacloudstack_api_gateway_app.default.9"
+	ra := resourceAttrInit(resourceId, apigatewayAppBasicMap)
+	serviceFunc := func() interface{} {
+		return &CloudApiService{testAccProvider.Meta().(*connectivity.AlibabacloudStackClient)}
+	}
+	rc := resourceCheckInit(resourceId, &v, serviceFunc)
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := getAccTestRandInt(1000000, 9999999)
+	name := fmt.Sprintf("tf_testAccApp_%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceApigatewayAppConfigDependence)
+
+	ResourceTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name":        "${var.name}",
+					"description": "${var.description}",
+					"count":       "10",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"name": name,
+					}),
+				),
+			},
+		},
+	})
+}
+
+func resourceApigatewayAppConfigDependence(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
   default = "%s"
 }
-resource "alibabacloudstack_api_gateway_group" "default" {
-  name        = "${var.name}"
-  description = "tf_testAccApiGroup Description"
-}
-resource "alibabacloudstack_api_gateway_api" "default" {
-  name        = "${var.name}"
-  group_id    = "${alibabacloudstack_api_gateway_group.default.id}"
-  description = "description"
-  auth_type   = "APP"
 
-  request_config {
-    protocol = "HTTP"
-    method   = "GET"
-    path     = "/test/path"
-    mode     = "MAPPING"
-  }
-
-  service_type = "HTTP"
-
-  http_service_config {
-    address   = "http://apigateway-backend.alicloudapi.com:8080"
-    method    = "GET"
-    path      = "/web/cloudapi"
-    timeout   = 22
-    aone_name = "cloudapi-openapi"
-  }
-
-  request_parameters {
-      name         = "aa"
-      type         = "STRING"
-      required     = "OPTIONAL"
-      in           = "QUERY"
-      in_service   = "QUERY"
-      name_service = "testparams"
-    }
+variable "description" {
+  default = "tf_testAcc api gateway description"
 }
 
-resource "alibabacloudstack_api_gateway_app" "default" {
-  name        = "${var.name}"
-  description = "tf_testAccApiAPP Description"
+`, name)
 }
 
- `, name)
-}
-
-var apigatewayAppAttachmentBasicMap = map[string]string{
-	"api_id":     CHECKSET,
-	"group_id":   CHECKSET,
-	"stage_name": "PRE",
-	"app_id":     CHECKSET,
+var apigatewayAppBasicMap = map[string]string{
+	"name": CHECKSET,
 }
