b83572c4058727aa4a47e87c95a45856ae18e2d0
儆钰 <jingyu.wy@alibaba-inc.com>
2025-04-22 20:17:56 +0800

    alibabacloudstack_ascm_user的init_passwordGi不至此import

--- alibabacloudstack/resource_apsarastack_ascm_user_test.go
+++ alibabacloudstack/resource_apsarastack_ascm_user_test.go
@@ -66,7 +66,7 @@ func TestAccAlibabacloudStackAscm_UserBasic(t *testing.T) {
 				ResourceName:            resourceId,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				//ImportStateVerifyIgnore: []string{"compact_topic", "partition_num", "remark"},
+				ImportStateVerifyIgnore: []string{"init_password"},
 			},
 		},
 	})
