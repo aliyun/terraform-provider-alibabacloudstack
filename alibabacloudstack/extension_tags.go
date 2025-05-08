6a59c3905791f2006265e337864b9166a43a5b1e
儆钰 <jingyu.wy@alibaba-inc.com>
2025-05-08 13:51:54 +0800

    移除对非标三方库denverdino/aliyungo的依赖

--- alibabacloudstack/extension_tags.go
+++ alibabacloudstack/extension_tags.go
@@ -1,8 +1,5 @@
 package alibabacloudstack
 
-import (
-	"github.com/denverdino/aliyungo/ecs"
-)
 
 type Tag struct {
 	Key   string `json:"key"`
@@ -12,18 +9,4 @@ type Tag struct {
 type JsonTag struct {
 	TagKey   string
 	TagValue string
-}
-
-type AddTagsArgs struct {
-	ResourceId   string
-	ResourceType ecs.TagResourceType //image, instance, snapshot or disk
-	RegionId     string
-	Tag          []Tag
-}
-
-type RemoveTagsArgs struct {
-	ResourceId   string
-	ResourceType ecs.TagResourceType //image, instance, snapshot or disk
-	RegionId     string
-	Tag          []Tag
 }
--- local_vendor/github.com/denverdino/aliyungo/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/denverdino/aliyungo
-
-go 1.21.4
