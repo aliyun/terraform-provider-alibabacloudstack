f6e849bfc187ec359440311486acde09eef7ac69
儆钰 <jingyu.wy@alibaba-inc.com>
2025-07-02 18:29:06 +0800

    合并nas service文件

--- alibabacloudstack/service_apsarastack_nas.go
+++ alibabacloudstack/service_apsarastack_nas.go
@@ -2,6 +2,8 @@ package alibabacloudstack
 
 import (
 	"fmt"
+	"strings"
+	"encoding/json"
 
 	"github.com/PaesslerAG/jsonpath"
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/connectivity"
@@ -187,4 +189,48 @@ func (s *NasService) DescribeNasFileSystemStateRefreshFunc(id string, defaultRet
 		}
 		return object, fmt.Sprint(object["Status"]), nil
 	}
+}
+
+
+type NasDescribelifecyclepoliciesResponse struct {
+	LifecyclePolicies struct {
+		LifecyclePolicy []struct {
+			FileSystemId        string `json:"FileSystemId"`
+			LifecyclePolicyName string `json:"LifecyclePolicyName"`
+			Path                string `json:"Path"`
+			Recursive           bool   `json:"Recursive"`
+			LifecycleRuleName   string `json:"LifecycleRuleName"`
+			StorageType         string `json:"StorageType"`
+			CreateTime          string `json:"CreateTime"`
+			OssBucket           string `json:"OssBucket"`
+		} `json:"LifecyclePolicy"`
+	} `json:"LifecyclePolicies"`
+	RequestId  string `json:"RequestId"`
+	TotalCount int    `json:"TotalCount"`
+	PageSize   int    `json:"PageSize"`
+	PageNumber int    `json:"PageNumber"`
+}
+
+func (s *NasService) DoNasDescribelifecyclepoliciesRequest(id string) (*NasDescribelifecyclepoliciesResponse, error) {
+	// api: NAS - 2017-06-26 - DescribeLifecyclePolicies
+	request := s.client.NewCommonRequest("GET", "NAS", "2017-06-26", "DescribeLifecyclePolicies", "")
+	NasDescribelifecyclepoliciesResponseObj := &NasDescribelifecyclepoliciesResponse{}
+	params := strings.Split(id, ":")
+	request.QueryParams["FileSystemId"] = params[0]
+	bresponse, err := s.client.ProcessCommonRequest(request)
+	if err != nil {
+		if bresponse == nil {
+			return nil, errmsgs.WrapErrorf(err, "Process Common Request Failed")
+		}
+		errmsg := errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
+		return nil, errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "", "DescribeLifecyclePolicies", request.GetActionName(), errmsgs.AlibabacloudStackSdkGoERROR, errmsg)
+	}
+
+	err = json.Unmarshal(bresponse.GetHttpContentBytes(), &NasDescribelifecyclepoliciesResponseObj)
+
+	if err != nil {
+		return nil, errmsgs.WrapErrorf(err, errmsgs.DefaultErrorMsg, "", "DescribeLifecyclePolicies", errmsgs.AlibabacloudStackSdkGoERROR)
+	}
+
+	return NasDescribelifecyclepoliciesResponseObj, nil
 }
