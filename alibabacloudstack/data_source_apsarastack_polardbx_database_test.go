d7b336acaa8bfe345f1a58aa8b167921f5ee5929
儆钰 <jingyu.wy@alibaba-inc.com>
2025-08-11 18:40:06 +0800

    fix polardb database unittest

--- alibabacloudstack/data_source_apsarastack_polardbx_database_test.go
+++ alibabacloudstack/data_source_apsarastack_polardbx_database_test.go
@@ -74,26 +74,12 @@ variable "name" {
   default = "%s"
 }
 
-variable "password" {
-  default = "%s"
-}
+%s
 
-resource "alibabacloudstack_polardbx_instance" "default" {
-    description = "testtf1111"
-	zone_id = "${data.alibabacloudstack_zones.default.zones.0.id}"
-	engine_version = "5.7"
-	storage = "50"
-	network_type = "vpc"
-	vpc_id = "${alibabacloudstack_vpc_vpc.default.id}"
-	vswitch_id = "${alibabacloudstack_vpc_vswitch.default.id}"
-	cn_node_class = "polarx.x4.medium.2e"
-	cn_node_count = "2"
-	dn_node_class = "mysql.n4.medium.25"
-	dn_node_count = "2"
-}
+%s
 	
 resource "alibabacloudstack_polardbx_database" "default" {
-    instance_id  = "${alibabacloudstack_polardbx_instance.default.id}"
+    instance_id  = "${local.polardbx_instance.id}"
 	database_name = "${var.name}"
 	encode = "utf8mb4"
 	mode = "auto"
@@ -102,6 +88,6 @@ resource "alibabacloudstack_polardbx_database" "default" {
 data "alibabacloudstack_polardbx_databases" "default" {
   %s
 }
-`, name, getAccTestPassword(12), strings.Join(pairs, "\n  "))
+`, name, VSwitchCommonTestCase, PolardbxReadOrCreateCommonTestCase(), strings.Join(pairs, "\n  "))
 	return config
 }
