5bdb7eb0334a7044975eadbfa4541223bc008f8d
儆钰 <jingyu.wy@alibaba-inc.com>
2025-08-25 14:30:29 +0800

    fix TestAccAlibabacloudStackCenTransitMulticastDomainSourcesDataSourceBasic test case

--- alibabacloudstack/data_source_apsarastack_cen_transit_router_multicast_domain_sources_test.go
+++ alibabacloudstack/data_source_apsarastack_cen_transit_router_multicast_domain_sources_test.go
@@ -56,93 +56,84 @@ func testAccCheckAlibabacloudStacMulticastDomainSourcesDataSourceConfig(rand int
 		pairs = append(pairs, k+" = "+v)
 	}
 
-	config := DataAlibabacloudstackVswitchZones + DataAlibabacloudstackImages + fmt.Sprintf(`
+	config := fmt.Sprintf(`
 	variable "name" {
 		default = "datasource_multicast_domain_source%d"
 	}
-	resource "alibabacloudstack_vpc" "vpc" {
-		name       = var.name
-		cidr_block = "192.168.0.0/24"
-	
+
+	%s
+
+	%s
+
+	data "alibabacloudstack_instance_types" "all" {
+	  availability_zone = data.alibabacloudstack_zones.default.zones[0].id
+	  sorted_by         = "CPU"
+	  eni_amount        = 2
+	}
+
+	resource "alibabacloudstack_ecs_instance" "default" {
+	  image_id             = "${data.alibabacloudstack_images.default.images.0.id}"
+	  instance_type        = data.alibabacloudstack_instance_types.all.instance_types.0.id
+	  system_disk_category = "${data.alibabacloudstack_zones.default.zones.0.available_disk_categories.0}"
+	  system_disk_size     = 20
+	  system_disk_name     = "test_sys_disk"
+	  security_groups      = [alibabacloudstack_ecs_securitygroup.default.id]
+	  instance_name        = "${var.name}_ecs"
+	  vswitch_id           = alibabacloudstack_vpc_vswitch.default.id
+	  zone_id    		   = data.alibabacloudstack_zones.default.zones.0.id
+	  is_outdated          = false
+	  lifecycle {
+	    ignore_changes = [
+	      instance_type
+	    ]
 	  }
-	  
-	  resource "alibabacloudstack_vswitch" "vswitch" {
-		name              = var.name
-		cidr_block        = "192.168.0.0/24"
-		availability_zone = data.alibabacloudstack_zones.default.zones[0].id
-		vpc_id            = alibabacloudstack_vpc.vpc.id
-	
-	  }
-	  
-	  resource "alibabacloudstack_security_group" "group" {
-		name   = var.name
-		vpc_id = alibabacloudstack_vpc.vpc.id
-	
-	  }
-	
-	data "alibabacloudstack_instance_types" "instance_type" {
-		availability_zone = data.alibabacloudstack_zones.default.zones[0].id
-		eni_amount        = 2
-		sorted_by         = "Memory"
-	  }
-	  
-	  resource "alibabacloudstack_instance" "instance" {
-		availability_zone = data.alibabacloudstack_zones.default.zones[0].id
-		security_groups   = [alibabacloudstack_security_group.group.id]
-		instance_type              = data.alibabacloudstack_instance_types.instance_type.instance_types[0].id
-		system_disk_category       = "cloud_efficiency"
-		image_id                   = data.alibabacloudstack_images.default.images[0].id
-		instance_name              = var.name
-		vswitch_id                 = alibabacloudstack_vswitch.vswitch.id
-	
-	  
-	  }
-	
+	}
+
 	  resource "alibabacloudstack_network_interface" "interface" {
 		name            = var.name
-		vswitch_id      = alibabacloudstack_vswitch.vswitch.id
-		security_groups = [alibabacloudstack_security_group.group.id]
-	
+		vswitch_id      = alibabacloudstack_vpc_vswitch.default.id
+		security_groups = [alibabacloudstack_ecs_securitygroup.default.id]
+
 	  }
 	  
 	  resource "alibabacloudstack_network_interface_attachment" "attachment" {
-		instance_id          = alibabacloudstack_instance.instance.id
+		instance_id          = alibabacloudstack_ecs_instance.default.id
 		network_interface_id = alibabacloudstack_network_interface.interface.id
-	
+
 	  }
-	
+
 	resource "alibabacloudstack_cen_instance" "default" {
 		cen_instance_name = "${var.name}"
 		description = "${var.name}"
 		transit_router_name = "${var.name}"
 		transit_router_description = "${var.name}"
-	
+
 	}
-	
+
 	resource "alibabacloudstack_cen_transit_router_vpc_attachment" "default" {
 		transit_router_attachment_name = "${var.name}"
 		transit_router_attachment_description = "${var.name}"
 		cen_id = "${alibabacloudstack_cen_instance.default.id}"
-		vpc_id = "${alibabacloudstack_vswitch.vswitch.vpc_id}"
+		vpc_id = "${alibabacloudstack_vpc_vswitch.default.vpc_id}"
 		transit_router_id = "${alibabacloudstack_cen_instance.default.transit_router_id}"
 		zone_mappings {
-				vswitch_id = "${alibabacloudstack_vswitch.vswitch.id}"
-				 zone_id = "${alibabacloudstack_vswitch.vswitch.availability_zone}"
+				vswitch_id = "${alibabacloudstack_vpc_vswitch.default.id}"
+				 zone_id = "${alibabacloudstack_vpc_vswitch.default.availability_zone}"
 			}
 	}
-	
+
 	resource "alibabacloudstack_cen_transit_router_multicast_domain" "default" {
 		transit_router_multicast_domain_description = "${var.name}"
 		transit_router_multicast_domain_name = "${var.name}"
 		transit_router_id = "${alibabacloudstack_cen_instance.default.transit_router_id}"
-	
+
 	}
-	
+
 	resource "alibabacloudstack_cen_transit_router_multicast_domain_association" "default" {
 		transit_router_multicast_domain_id = "${alibabacloudstack_cen_transit_router_multicast_domain.default.transit_router_multicast_domain_id}"
 		transit_router_attachment_id = "${alibabacloudstack_cen_transit_router_vpc_attachment.default.transit_router_attachment_id}"
-		vswitch_id = "${alibabacloudstack_vswitch.vswitch.id}"
-	
+		vswitch_id = "${alibabacloudstack_vpc_vswitch.default.id}"
+
 	}
 
 	resource "alibabacloudstack_cen_transit_router_multicast_domain_source" "default" {
@@ -156,7 +147,7 @@ func testAccCheckAlibabacloudStacMulticastDomainSourcesDataSourceConfig(rand int
 data "alibabacloudstack_cen_transit_router_multicast_domain_sources" "default" {
 	transit_router_multicast_domain_id="${alibabacloudstack_cen_transit_router_multicast_domain_source.default.transit_router_multicast_domain_id}"
 	%s
-}`, rand, strings.Join(pairs, "\n  "))
+}`, rand, SecurityGroupCommonTestCase , DataAlibabacloudstackImages, strings.Join(pairs, "\n  "))
 	return config
 }
 
