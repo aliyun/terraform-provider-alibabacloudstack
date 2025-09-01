13d00293ec481673ca0178869218ee3d72c6fd2b
儆钰 <jingyu.wy@alibaba-inc.com>
2025-09-01 16:53:39 +0800

    Creating MaxCompute users across organizations is not allowed.

--- alibabacloudstack/data_source_apsarastack_maxcompute_projects_test.go
+++ alibabacloudstack/data_source_apsarastack_maxcompute_projects_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccAlibabacloudStackAscmMaxcomputeProjectDataSource(t *testing.T) {
+func TestAccAlibabacloudStackMaxcomputeProjectsDataSource(t *testing.T) {
 	ResourceTest(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
