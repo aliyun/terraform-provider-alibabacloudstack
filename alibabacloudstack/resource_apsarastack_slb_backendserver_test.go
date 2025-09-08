51b32f7747847e95e6405f1c57431775375155b9
儆钰 <jingyu.wy@alibaba-inc.com>
2025-09-08 20:04:13 +0800

    fix slb backend server testcase

--- alibabacloudstack/resource_apsarastack_slb_backendserver_test.go
+++ alibabacloudstack/resource_apsarastack_slb_backendserver_test.go
@@ -49,7 +49,6 @@ func TestAccAlibabacloudStackSlbBackendServers_vpc(t *testing.T) {
 						"backend_servers.#": "1",
 					}),
 				),
-				ExpectNonEmptyPlan: true,
 			},
 			{
 				ResourceName:            resourceId,
@@ -108,7 +107,6 @@ func TestAccAlibabacloudStackSlbBackendServers_multi_vpc(t *testing.T) {
 						"backend_servers.#": "2",
 					}),
 				),
-				ExpectNonEmptyPlan: true,
 			},
 		},
 	})
@@ -159,7 +157,6 @@ func TestAccAlibabacloudStackSlbBackendServers_classic(t *testing.T) {
 						"backend_servers.#": "2",
 					}),
 				),
-				ExpectNonEmptyPlan: true,
 			},
 			{
 				ResourceName:            resourceId,
@@ -237,40 +234,24 @@ resource "alibabacloudstack_slb" "default" {
   vswitch_id    = "${alibabacloudstack_vpc_vswitch.default.id}"
 }
 
-
-data "alibabacloudstack_instance_types" "new" {
- 	availability_zone = "${data.alibabacloudstack_zones.default.zones.0.id}"
-	eni_amount = 2
-}
-
-resource "alibabacloudstack_network_interface" "default" {
-    count = 1
-    name = "${var.name}"
-    vswitch_id = "${alibabacloudstack_vpc_vswitch.default.id}"
-    security_groups = [alibabacloudstack_ecs_securitygroup.default.id]
-}
-resource "alibabacloudstack_instance" "new" {
-	image_id             = "${data.alibabacloudstack_images.default.images.0.id}"
-	instance_type        = "${data.alibabacloudstack_instance_types.new.instance_types[0].id}"
-	system_disk_category = "${data.alibabacloudstack_zones.default.zones.0.available_disk_categories.0}"
-	system_disk_size     = 40
-	system_disk_name     = "test_sys_disk"
-	security_groups      =	[alibabacloudstack_ecs_securitygroup.default.id]
-	instance_name        = "${var.name}_ecs"
-	vswitch_id           = alibabacloudstack_vpc_vswitch.default.id
-	zone_id    = data.alibabacloudstack_zones.default.zones.0.id
-	is_outdated          = false
-	lifecycle {
-	  ignore_changes = [
-		instance_type
-	  ]
-	}
+resource "alibabacloudstack_ecs_instance" "new" {
+  image_id             = "${data.alibabacloudstack_images.default.images.0.id}"
+  instance_type        = "${local.default_instance_type_id}"
+  system_disk_category = "${data.alibabacloudstack_zones.default.zones.0.available_disk_categories.0}"
+  system_disk_size     = 20
+  system_disk_name     = "test_sys_disk"
+  security_groups      = [alibabacloudstack_ecs_securitygroup.default.id]
+  instance_name        = "${var.name}_ecs"
+  vswitch_id           = alibabacloudstack_vpc_vswitch.default.id
+  zone_id    		   = data.alibabacloudstack_zones.default.zones.0.id
+  is_outdated          = false
+  lifecycle {
+    ignore_changes = [
+      instance_type
+    ]
   }
-resource "alibabacloudstack_network_interface_attachment" "default" {
-	count = 1
-    instance_id = "${alibabacloudstack_instance.new.id}"
-    network_interface_id = "${element(alibabacloudstack_network_interface.default.*.id, count.index)}"
 }
+
 `, name, ECSInstanceCommonTestCase)
 }
 
