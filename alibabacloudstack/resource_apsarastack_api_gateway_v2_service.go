a60149991ace3254896884c931e534e1515161d8
dwt01285077 <dwt01285077@alibaba-inc.com>
2025-11-14 11:01:09 +0800

    apigw_v2_service add type: database

--- alibabacloudstack/resource_apsarastack_api_gateway_v2_service.go
+++ alibabacloudstack/resource_apsarastack_api_gateway_v2_service.go
@@ -21,6 +21,7 @@ func resourceAlibabacloudStackAPIGatewayV2Service() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"description": {
 				Type:     schema.TypeString,
@@ -28,19 +29,16 @@ func resourceAlibabacloudStackAPIGatewayV2Service() *schema.Resource {
 			},
 			"upstream_type": {
 				Type:     schema.TypeInt,
-				Required: true,
+				Optional: true,
 			},
 			"load_balance_type": {
 				Type:     schema.TypeInt,
-				Required: true,
+				Optional: true,
 			},
 			"protocol": {
 				Type:     schema.TypeString,
-				Required: true,
-			},
-			"service_name": {
-				Type:     schema.TypeString,
 				Optional: true,
+				Computed: true,
 			},
 			"real_service_name": {
 				Type:     schema.TypeString,
@@ -51,6 +49,26 @@ func resourceAlibabacloudStackAPIGatewayV2Service() *schema.Resource {
 				Optional: true,
 			},
 			"service_version": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"source_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"max_limit": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"timeout": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"is_scan_protect": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"original_sql": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
@@ -127,6 +145,67 @@ func resourceAlibabacloudStackAPIGatewayV2Service() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"sql_input_parameters": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"original_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"target_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"isoptional": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"description": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"sample": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+			"sql_output_parameters": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"original_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"target_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"param_type": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "java.lang.String",
+						},
+						"isoptional": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"description": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"sample": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
 		},
 	}
 	setResourceFunc(resource, resourceAlibabacloudStackAPIGatewayV2ServiceCreate, resourceAlibabacloudStackAPIGatewayV2ServiceRead, resourceAlibabacloudStackAPIGatewayV2ServiceUpdate, resourceAlibabacloudStackAPIGatewayV2ServiceDelete)
@@ -137,40 +216,55 @@ func resourceAlibabacloudStackAPIGatewayV2ServiceCreate(d *schema.ResourceData, 
 	client := meta.(*connectivity.AlibabacloudStackClient)
 
 	request := make(map[string]interface{})
+	service_type := d.Get("service_type").(int)
 	request["name"] = d.Get("name")
 	request["description"] = d.Get("description")
-	request["upstreamType"] = d.Get("upstream_type")
-	request["loadBalanceType"] = d.Get("load_balance_type")
-	request["protocol"] = d.Get("protocol")
-	request["serviceName"] = d.Get("service_name")
-	request["serviceType"] = d.Get("service_type")
-	request["healthCheckStruct"] = expandHealthCheckStruct(d.Get("health_check_struct"))
+	request["serviceType"] = service_type
 	request["gwInstanceId"] = d.Get("gw_instance_id")
-	serviceStruct := make(map[string]interface{})
-	serviceStruct["realServiceName"] = d.Get("real_service_name")
-	serviceStruct["group"] = d.Get("service_group")
-	serviceStruct["version"] = d.Get("service_version")
-	nodes := d.Get("service_nodes").([]interface{})
-	nodeList := make([]map[string]interface{}, 0, len(nodes))
-	for _, node := range nodes {
-		n := node.(map[string]interface{})
-		nodeMap := make(map[string]interface{})
-		nodeMap["ip"] = n["ip"]
-		nodeMap["port"] = n["port"]
-		nodeMap["weight"] = n["weight"]
-		nodeMap["enable"] = n["enable"]
-		nodeList = append(nodeList, nodeMap)
-	}
-	serviceStruct["nodes"] = nodeList
-	request["serviceStruct"] = serviceStruct
-	healthCheckStruct := expandHealthCheckStruct(d.Get("health_check_struct"))
-	if healthCheckStruct != nil {
-		request["healthCheckStruct"] = healthCheckStruct
-		request["isOpenHealthCheck"] = true
-	} else {
-		request["isOpenHealthCheck"] = false
-	}
+	request["protocol"] = d.Get("protocol")
+	switch service_type {
+	case 0:
+		request["healthCheckStruct"] = expandHealthCheckStruct(d.Get("health_check_struct"))
+		request["upstreamType"] = d.Get("upstream_type")
+		request["loadBalanceType"] = d.Get("load_balance_type")
+		serviceStruct := make(map[string]interface{})
+		serviceStruct["realServiceName"] = d.Get("real_service_name")
+		serviceStruct["group"] = d.Get("service_group")
+		serviceStruct["version"] = d.Get("service_version")
+		nodes := d.Get("service_nodes").([]interface{})
+		nodeList := make([]map[string]interface{}, 0, len(nodes))
+		for _, node := range nodes {
+			n := node.(map[string]interface{})
+			nodeMap := make(map[string]interface{})
+			nodeMap["ip"] = n["ip"]
+			nodeMap["port"] = n["port"]
+			nodeMap["weight"] = n["weight"]
+			nodeMap["enable"] = n["enable"]
+			nodeList = append(nodeList, nodeMap)
+		}
+		serviceStruct["nodes"] = nodeList
+		request["serviceStruct"] = serviceStruct
+		healthCheckStruct := expandHealthCheckStruct(d.Get("health_check_struct"))
+		if healthCheckStruct != nil {
+			request["healthCheckStruct"] = healthCheckStruct
+			request["isOpenHealthCheck"] = true
+		} else {
+			request["isOpenHealthCheck"] = false
+		}
+	case 1:
+		request["sourceId"] = d.Get("source_id")
+		request["maxLimit"] = d.Get("max_limit")
+		request["timeout"] = d.Get("timeout")
+		request["isScanProtect"] = d.Get("is_scan_protect")
+		request["originalSql"] = d.Get("original_sql")
 
+		inputParams := d.Get("sql_input_parameters").(*schema.Set).List()
+		request["sqlInputParameters"] = buildSqlInputParams(inputParams)
+		outputParams := d.Get("sql_output_parameters").(*schema.Set).List()
+		request["sqlInputParameters"] = buildSqlOutputParams(outputParams)
+	default:
+
+	}
 	resp, err := client.DoTeaRequest("POST", "csb2", "2023-02-06", "CreateService", "/microservice/createService", nil, nil, request)
 	if err != nil {
 		return errmsgs.WrapErrorf(err, errmsgs.DefaultErrorMsg, "alibabacloudstack_api_gateway_v2_service", "CreateService", errmsgs.AlibabacloudStackSdkGoERROR)
@@ -215,22 +309,22 @@ func resourceAlibabacloudStackAPIGatewayV2ServiceRead(d *schema.ResourceData, me
 		}
 		return err
 	}
-
-	d.Set("gw_instance_id", object["gwInstanceId"])
+	parts, err := ParseResourceId(d.Id(), 2)
+	if err != nil {
+		return errmsgs.WrapError(err)
+	}
+	gwInstanceId := parts[0]
+	d.Set("gw_instance_id", gwInstanceId)
 	d.Set("name", object["name"])
 	d.Set("description", object["description"])
 	d.Set("upstream_type", object["upstreamType"])
 	d.Set("load_balance_type", object["loadBalanceType"])
 	d.Set("protocol", object["protocol"])
-	d.Set("service_name", object["serviceName"])
 	d.Set("service_type", object["serviceType"])
 	d.Set("service_id", object["serviceId"])
 
 	if serviceStruct, ok := object["serviceStruct"]; ok && serviceStruct != nil {
 		serviceStructMap := serviceStruct.(map[string]interface{})
-		d.Set("real_service_name", serviceStructMap["realServiceName"])
-		d.Set("service_group", serviceStructMap["group"])
-		d.Set("service_version", serviceStructMap["version"])
 
 		if nodes, ok := serviceStructMap["nodes"].([]interface{}); ok && len(nodes) > 0 {
 			nodeList := make([]map[string]interface{}, 0, len(nodes))
@@ -245,6 +339,38 @@ func resourceAlibabacloudStackAPIGatewayV2ServiceRead(d *schema.ResourceData, me
 				nodeList = append(nodeList, nodeData)
 			}
 			d.Set("service_nodes", nodeList)
+		}
+		if v, ok := serviceStructMap["realServiceName"]; ok {
+			d.Set("real_service_name", v)
+		}
+		if v, ok := serviceStructMap["group"]; ok {
+			d.Set("service_group", v)
+		}
+		if v, ok := serviceStructMap["version"]; ok {
+			d.Set("service_version", v)
+		}
+		if v, ok := serviceStructMap["sourceId"]; ok {
+			d.Set("source_id", v)
+		}
+		if v, ok := serviceStructMap["maxLimit"]; ok {
+			d.Set("max_limit", v)
+		}
+		if v, ok := serviceStructMap["timeout"]; ok {
+			d.Set("timeout", v)
+		}
+		if v, ok := serviceStructMap["isScanProtect"]; ok {
+			d.Set("is_scan_protect", v)
+		}
+		if v, ok := serviceStructMap["originalSql"]; ok {
+			d.Set("original_sql", v)
+		}
+		if v, ok := serviceStructMap["inputParameterList"]; ok {
+			inputParams := readSqlInputParams(v.([]interface{}))
+			d.Set("sql_input_parameters", inputParams)
+		}
+		if v, ok := serviceStructMap["outputParameterList"]; ok {
+			outputParams := readSqlOutputParams(v.([]interface{}))
+			d.Set("sql_output_parameters", outputParams)
 		}
 	}
 
@@ -288,37 +414,54 @@ func resourceAlibabacloudStackAPIGatewayV2ServiceUpdate(d *schema.ResourceData, 
 	request["gwInstanceId"] = gwInstanceId
 
 	// Set all fields that can be updated
+	service_type := d.Get("service_type").(int)
 	request["name"] = d.Get("name")
 	request["description"] = d.Get("description")
-	request["upstreamType"] = d.Get("upstream_type")
-	request["loadBalanceType"] = d.Get("load_balance_type")
+	request["serviceType"] = service_type
 	request["protocol"] = d.Get("protocol")
-	request["serviceName"] = d.Get("service_name")
-	serviceStruct := make(map[string]interface{})
-	serviceStruct["realServiceName"] = d.Get("real_service_name")
-	serviceStruct["group"] = d.Get("service_group")
-	serviceStruct["version"] = d.Get("service_version")
-	nodes := d.Get("service_nodes").([]interface{})
-	nodeList := make([]map[string]interface{}, 0, len(nodes))
-	for _, node := range nodes {
-		n := node.(map[string]interface{})
-		nodeMap := make(map[string]interface{})
-		nodeMap["ip"] = n["ip"]
-		nodeMap["port"] = n["port"]
-		nodeMap["weight"] = n["weight"]
-		nodeMap["enable"] = n["enable"]
-		nodeList = append(nodeList, nodeMap)
-	}
-	serviceStruct["nodes"] = nodeList
-	request["serviceStruct"] = serviceStruct
-	request["serviceType"] = d.Get("service_type")
-	healthCheckStruct := expandHealthCheckStruct(d.Get("health_check_struct"))
-	if len(healthCheckStruct) > 0 {
-		request["healthCheckStruct"] = healthCheckStruct
-	} else {
-		request["healthCheckStruct"] = false
-	}
+	switch service_type {
+	case 0:
+		request["healthCheckStruct"] = expandHealthCheckStruct(d.Get("health_check_struct"))
+		request["upstreamType"] = d.Get("upstream_type")
+		request["loadBalanceType"] = d.Get("load_balance_type")
+		serviceStruct := make(map[string]interface{})
+		serviceStruct["realServiceName"] = d.Get("real_service_name")
+		serviceStruct["group"] = d.Get("service_group")
+		serviceStruct["version"] = d.Get("service_version")
+		nodes := d.Get("service_nodes").([]interface{})
+		nodeList := make([]map[string]interface{}, 0, len(nodes))
+		for _, node := range nodes {
+			n := node.(map[string]interface{})
+			nodeMap := make(map[string]interface{})
+			nodeMap["ip"] = n["ip"]
+			nodeMap["port"] = n["port"]
+			nodeMap["weight"] = n["weight"]
+			nodeMap["enable"] = n["enable"]
+			nodeList = append(nodeList, nodeMap)
+		}
+		serviceStruct["nodes"] = nodeList
+		request["serviceStruct"] = serviceStruct
+		healthCheckStruct := expandHealthCheckStruct(d.Get("health_check_struct"))
+		if healthCheckStruct != nil {
+			request["healthCheckStruct"] = healthCheckStruct
+			request["isOpenHealthCheck"] = true
+		} else {
+			request["isOpenHealthCheck"] = false
+		}
+	case 1:
+		request["sourceId"] = d.Get("source_id")
+		request["maxLimit"] = d.Get("max_limit")
+		request["timeout"] = d.Get("timeout")
+		request["isScanProtect"] = d.Get("is_scan_protect")
+		request["originalSql"] = d.Get("original_sql")
 
+		inputParams := d.Get("sql_input_parameters").(*schema.Set).List()
+		request["sqlInputParameters"] = buildSqlInputParams(inputParams)
+		outputParams := d.Get("sql_output_parameters").(*schema.Set).List()
+		request["sqlInputParameters"] = buildSqlOutputParams(outputParams)
+	default:
+
+	}
 	_, err = client.DoTeaRequest("POST", "csb2", "2023-02-06", "ModifyService", "/microservice/modifyService", nil, nil, request)
 	if err != nil {
 		return errmsgs.WrapErrorf(err, errmsgs.DefaultErrorMsg, "alibabacloudstack_api_gateway_v2_service", "ModifyService", errmsgs.AlibabacloudStackSdkGoERROR)
@@ -356,4 +499,156 @@ func resourceAlibabacloudStackAPIGatewayV2ServiceDelete(d *schema.ResourceData, 
 		return errmsgs.WrapError(err)
 	}
 	return nil
+}
+
+func buildSqlInputParams(input_parameters []interface{}) []map[string]interface{} {
+	input_fields := make([]map[string]interface{}, 0)
+	for _, v := range input_parameters {
+		params := v.(map[string]interface{})
+		input_fields = append(input_fields, map[string]interface{}{
+			"originalName": params["original_name"],
+			"targetName":   params["target_name"],
+			"optional":     params["isoptional"],
+			"sample":       params["sample"],
+			"description":  params["description"],
+			"fields":       []string{},
+		})
+	}
+	input := map[string]interface{}{
+		"originalName": "queryValues",
+		"targetName":   "queryValues",
+		"paramType":    "QueryValues",
+		"optional":     false,
+		"sample":       "",
+		"description":  "查询条件字段集",
+		"fields":       input_fields,
+	}
+	result := []map[string]interface{}{
+		{
+			"originalName": "requireTotalCount",
+			"targetName":   "requireTotalCount",
+			"paramType":    "java.lang.Boolean",
+			"optional":     true,
+		},
+		{
+			"originalName": "offset",
+			"targetName":   "offset",
+			"paramType":    "java.lang.Integer",
+			"optional":     true,
+		},
+		{
+			"originalName": "limit",
+			"targetName":   "limit",
+			"paramType":    "java.lang.Integer",
+			"optional":     true,
+		},
+		input,
+	}
+	return result
+}
+
+func buildSqlOutputParams(output_parameters []interface{}) []map[string]interface{} {
+	output_fields := []map[string]interface{}{
+		{
+			"originalName": "__csbRecordError",
+			"targetName":   "errorMsg",
+			"paramType":    "java.lang.String",
+			"optional":     true,
+		},
+	}
+	for _, v := range output_parameters {
+		params := v.(map[string]interface{})
+		output_fields = append(output_fields, map[string]interface{}{
+			"originalName": params["original_name"],
+			"targetName":   params["target_name"],
+			"optional":     params["isoptional"],
+			"sample":       params["sample"],
+			"description":  params["description"],
+			"fields":       []string{},
+		})
+	}
+	oupput := map[string]interface{}{
+		"originalName": "queryValues",
+		"targetName":   "queryValues",
+		"paramType":    "QueryValues",
+		"optional":     false,
+		"fields":       output_fields,
+	}
+	result := []map[string]interface{}{
+		{
+			"originalName": "resultCode",
+			"targetName":   "resultCode",
+			"paramType":    "java.lang.Integer",
+			"optional":     false,
+			"sample":       "",
+		},
+		{
+			"originalName": "resultMsg",
+			"targetName":   "resultMsg",
+			"paramType":    "java.lang.String",
+			"optional":     false,
+			"sample":       "",
+		},
+		{
+			"originalName": "resultCount",
+			"targetName":   "resultCount",
+			"paramType":    "java.lang.Integer",
+			"optional":     true,
+			"sample":       "",
+		},
+		{
+			"originalName": "count",
+			"targetName":   "count",
+			"paramType":    "java.lang.Integer",
+			"optional":     false,
+			"sample":       "",
+		},
+		oupput,
+	}
+	return result
+}
+
+func readSqlInputParams(inputParams []interface{}) []map[string]interface{} {
+	input := make([]map[string]interface{}, 0)
+	for _, v := range inputParams {
+		params := v.(map[string]interface{})
+		if params["originalName"] == "queryValues" {
+			fields := params["fields"].([]interface{})
+			for _, field := range fields {
+				fieldParams := field.(map[string]interface{})
+				input = append(input, map[string]interface{}{
+					"original_name": fieldParams["originalName"],
+					"target_name":   fieldParams["targetName"],
+					"isoptional":    fieldParams["optional"],
+					"sample":        fieldParams["sample"],
+					"description":   fieldParams["description"],
+				})
+			}
+		}
+	}
+	return input
+}
+
+func readSqlOutputParams(outputParams []interface{}) []map[string]interface{} {
+	output := make([]map[string]interface{}, 0)
+	for _, v := range outputParams {
+		params := v.(map[string]interface{})
+		if params["originalName"] == "result" {
+			fields := params["fields"].([]interface{})
+			for _, v := range fields {
+				fieldParams := v.(map[string]interface{})
+				if fieldParams["originalName"] == "__csbRecordError" {
+					continue
+				}
+				output = append(output, map[string]interface{}{
+					"original_name": fieldParams["originalName"],
+					"target_name":   fieldParams["targetName"],
+					"isoptional":    fieldParams["optional"],
+					"sample":        fieldParams["sample"],
+					"description":   fieldParams["description"],
+				})
+			}
+		}
+	}
+	return output
 }
