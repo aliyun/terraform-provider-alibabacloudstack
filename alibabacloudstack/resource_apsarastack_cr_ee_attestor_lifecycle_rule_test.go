19a2596173c5e06760c7d54056c81fa5ca18498a
dwt01285077 <dwt01285077@alibaba-inc.com>
2026-01-04 16:47:47 +0800

    add a new testcase

--- alibabacloudstack/resource_apsarastack_cr_ee_attestor_lifecycle_rule_test.go
+++ alibabacloudstack/resource_apsarastack_cr_ee_attestor_lifecycle_rule_test.go
@@ -81,6 +81,78 @@ func TestAccAlibabacloudStackCrEEArtifactLifecycleRule_basic(t *testing.T) {
 	})
 }
 
+func TestAccAlibabacloudStackCrEEArtifactLifecycleRule_Repo(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alibabacloudstack_cr_ee_attestor_lifecycle_rule.default"
+	ra := resourceAttrInit(resourceId, crRepoMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CrService{testAccProvider.Meta().(*connectivity.AlibabacloudStackClient)}
+	}, "DescribeCrEEArtifactLifecycleRule")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := getAccTestRandInt(1000000, 9999999)
+	name := fmt.Sprintf("tf-testacc-cree-rule-%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceCrEEArtifactLifecycleRuleDependence)
+	ResourceTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"scope":               "REPO",
+					"instance_id":         "${data.alibabacloudstack_cr_ee_instances.default.instances.0.id}",
+					"retention_tag_count": "30",
+					"tag_regexp":          "release-v.*",
+					"enable_delete_tag":   "true",
+					"namespace_name":      "${alibabacloudstack_cr_ee_namespace.default.name}",
+					"repo_name":           "${var.name}",
+					"recent_pull_keep":    30,
+					"recent_push_keep":    20,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"scope":               "REPO",
+						"instance_id":         CHECKSET,
+						"retention_tag_count": "30",
+						"tag_regexp":          "release-v.*",
+						"enable_delete_tag":   "true",
+						"namespace_name":      name,
+						"repo_name":           name,
+						"recent_pull_keep":    "30",
+						"recent_push_keep":    "20",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"scope":               "NAMESPACE",
+					"namespace_name":      "${alibabacloudstack_cr_ee_namespace.default2.name}",
+					"retention_tag_count": "25",
+					"tag_regexp":          "release-2.*",
+					"enable_delete_tag":   "false",
+					"recent_pull_keep":    "10",
+					"recent_push_keep":    "10",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"scope":               "NAMESPACE",
+						"namespace_name":      name + "2",
+						"retention_tag_count": "25",
+						"tag_regexp":          "release-2.*",
+						"enable_delete_tag":   "false",
+						"recent_pull_keep":    "10",
+						"recent_push_keep":    "10",
+					}),
+				),
+			},
+		},
+	})
+}
+
 func resourceCrEEArtifactLifecycleRuleDependence(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
