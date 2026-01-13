92390001767461c244b936f6ddafdf48c1d92008
dwt01285077 <dwt01285077@alibaba-inc.com>
2026-01-13 16:52:36 +0800

    fix network_interface_attachment. testcase

--- alibabacloudstack/resource_apsarastack_ecs_networkinterfaceattachment_test.go
+++ alibabacloudstack/resource_apsarastack_ecs_networkinterfaceattachment_test.go
@@ -2,8 +2,9 @@ package alibabacloudstack
 
 import (
 	"fmt"
-	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/errmsgs"
 	"testing"
+
+	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/errmsgs"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 
@@ -15,14 +16,20 @@ import (
 func TestAccAlibabacloudStackNetworkInterfaceAttachmentBasic(t *testing.T) {
 	var v ecs.NetworkInterfaceSet
 	resourceId := "alibabacloudstack_network_interface_attachment.default"
-	ra := resourceAttrInit(resourceId, testAccCheckNetworkInterfaceAttachmentCheckMap)
-	serviceFunc := func() interface{} {
+	ra := resourceAttrInit(resourceId, map[string]string{
+		"instance_id":          CHECKSET,
+		"network_interface_id": CHECKSET,
+	})
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &EcsService{testAccProvider.Meta().(*connectivity.AlibabacloudStackClient)}
-	}
-	rc := resourceCheckInit(resourceId, &v, serviceFunc)
+	}, "DescribeNetworkInterfaceAttachment")
 	rac := resourceAttrCheckInit(rc, ra)
-
 	testAccCheck := rac.resourceAttrMapUpdateSet()
+
+	rand := getAccTestRandInt(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%secsnetwork_attachment%d", defaultRegionToTest, rand)
+
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlibabacloudTestAccEcsNetworkInterfaceAttachmentdependence)
 	ResourceTest(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -33,7 +40,10 @@ func TestAccAlibabacloudStackNetworkInterfaceAttachmentBasic(t *testing.T) {
 		CheckDestroy:  testAccCheckNetworkInterfaceAttachmentDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccNetworkInterfaceAttachmentConfigBasic,
+				Config: testAccConfig(map[string]interface{}{
+					"instance_id":          "${alibabacloudstack_ecs_instance.default.id}",
+					"network_interface_id": "${alibabacloudstack_network_interface.default.id}",
+				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(nil),
 				),
@@ -42,36 +52,6 @@ func TestAccAlibabacloudStackNetworkInterfaceAttachmentBasic(t *testing.T) {
 				ResourceName:      resourceId,
 				ImportState:       true,
 				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func TestAccAlibabacloudStackNetworkInterfaceAttachmentMulti(t *testing.T) {
-	var v ecs.NetworkInterfaceSet
-	resourceId := "alibabacloudstack_network_interface_attachment.default.1"
-	ra := resourceAttrInit(resourceId, testAccCheckNetworkInterfaceAttachmentCheckMap)
-	serviceFunc := func() interface{} {
-		return &EcsService{testAccProvider.Meta().(*connectivity.AlibabacloudStackClient)}
-	}
-	rc := resourceCheckInit(resourceId, &v, serviceFunc)
-	rac := resourceAttrCheckInit(rc, ra)
-
-	testAccCheck := rac.resourceAttrMapUpdateSet()
-	ResourceTest(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-
-		IDRefreshName: resourceId,
-		Providers:     testAccProviders,
-		CheckDestroy:  testAccCheckNetworkInterfaceAttachmentDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccNetworkInterfaceAttachmentConfigMulti,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(nil),
-				),
 			},
 		},
 	})
@@ -101,108 +81,20 @@ func testAccCheckNetworkInterfaceAttachmentDestroy(s *terraform.State) error {
 	return nil
 }
 
-const testAccNetworkInterfaceAttachmentConfigBasic = DataAlibabacloudstackVswitchZones + DataAlibabacloudstackInstanceTypes_Eni2 + DataAlibabacloudstackImages + `
-
+func AlibabacloudTestAccEcsNetworkInterfaceAttachmentdependence(name string) string {
+	return fmt.Sprintf(`
 variable "name" {
-  default = "tf-testAccNetworkInterfaceAttachment"
+    default = "%s"
 }
 
-resource "alibabacloudstack_vpc" "default" {
-    name = "${var.name}"
-    cidr_block = "192.168.0.0/24"
-}
-
-resource "alibabacloudstack_vswitch" "default" {
-    name = "${var.name}"
-    cidr_block = "192.168.0.0/24"
-    availability_zone = "${reverse(data.alibabacloudstack_zones.default.zones).0.id}"
-    vpc_id = "${alibabacloudstack_vpc.default.id}"
-}
-
-resource "alibabacloudstack_security_group" "default" {
-    name = "${var.name}"
-    vpc_id = "${alibabacloudstack_vpc.default.id}"
-}
-
-resource "alibabacloudstack_instance" "default" {
-    availability_zone = "${reverse(data.alibabacloudstack_zones.default.zones).0.id}"
-    security_groups = ["${alibabacloudstack_security_group.default.id}"]
-
-    instance_type = "${data.alibabacloudstack_instance_types.default.instance_types.0.id}"
-    system_disk_category = "cloud_efficiency"
-    image_id             = "${data.alibabacloudstack_images.default.images.0.id}"
-    instance_name        = "${var.name}"
-    vswitch_id = "${alibabacloudstack_vswitch.default.id}"
-    internet_max_bandwidth_out = 10
-}
+%s
 
 resource "alibabacloudstack_network_interface" "default" {
     name = "${var.name}"
-    vswitch_id = "${alibabacloudstack_vswitch.default.id}"
-    security_groups = [ "${alibabacloudstack_security_group.default.id}" ]
+    vswitch_id = "${alibabacloudstack_vpc_vswitch.default.id}"
+    security_groups = [ "${alibabacloudstack_ecs_securitygroup.default.id}" ]
 }
 
-resource "alibabacloudstack_network_interface_attachment" "default" {
-    instance_id = "${alibabacloudstack_instance.default.id}"
-    network_interface_id = "${alibabacloudstack_network_interface.default.id}"
-}
-`
 
-const testAccNetworkInterfaceAttachmentConfigMulti = DataAlibabacloudstackVswitchZones + DataAlibabacloudstackInstanceTypes_Eni2 + DataAlibabacloudstackImages + `
-
-variable "name" {
-  default = "tf-testAccNetworkInterfaceAttachment"
-}
-
-variable "number" {
-		default = "2"
-	}
-
-resource "alibabacloudstack_vpc" "default" {
-    name = "${var.name}"
-    cidr_block = "192.168.0.0/24"
-}
-
-resource "alibabacloudstack_vswitch" "default" {
-    name = "${var.name}"
-    cidr_block = "192.168.0.0/24"
-    availability_zone = "${reverse(data.alibabacloudstack_zones.default.zones).0.id}"
-    vpc_id = "${alibabacloudstack_vpc.default.id}"
-}
-
-resource "alibabacloudstack_security_group" "default" {
-    name = "${var.name}"
-    vpc_id = "${alibabacloudstack_vpc.default.id}"
-}
-
-resource "alibabacloudstack_instance" "default" {
-	count = "${var.number}"
-    availability_zone = "${reverse(data.alibabacloudstack_zones.default.zones).0.id}"
-    security_groups = ["${alibabacloudstack_security_group.default.id}"]
-
-    instance_type = "${data.alibabacloudstack_instance_types.default.instance_types.0.id}"
-    system_disk_category = "cloud_efficiency"
-    image_id             = "${data.alibabacloudstack_images.default.images.0.id}"
-    instance_name        = "${var.name}"
-    vswitch_id = "${alibabacloudstack_vswitch.default.id}"
-    internet_max_bandwidth_out = 10
-}
-
-resource "alibabacloudstack_network_interface" "default" {
-    count = "${var.number}"
-    name = "${var.name}"
-    vswitch_id = "${alibabacloudstack_vswitch.default.id}"
-    security_groups = [ "${alibabacloudstack_security_group.default.id}" ]
-}
-
-resource "alibabacloudstack_network_interface_attachment" "default" {
-	count = "${var.number}"
-    instance_id = "${element(alibabacloudstack_instance.default.*.id, count.index)}"
-    network_interface_id = "${element(alibabacloudstack_network_interface.default.*.id, count.index)}"
-}
-`
-
-var testAccCheckNetworkInterfaceAttachmentCheckMap = map[string]string{
-	"instance_id":          CHECKSET,
-	"network_interface_id": CHECKSET,
+`, name, ECSInstanceCommonTestCase)
 }
