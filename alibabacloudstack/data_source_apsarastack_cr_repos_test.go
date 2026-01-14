7847d0c2c5b15f2d08a89cca27cba5f208da16ef
jingyu <jingyu.wy@alibaba-inc.com>
2026-01-14 20:49:40 +0800

    fix cr repos datasource testcase

--- alibabacloudstack/data_source_apsarastack_cr_repos_test.go
+++ alibabacloudstack/data_source_apsarastack_cr_repos_test.go
@@ -2,56 +2,92 @@ package alibabacloudstack
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"testing"
 )
 
 func TestAccAlibabacloudStackCRReposDataSource(t *testing.T) {
-	ResourceTest(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: dataSourceCRReposConfigDataSource(),
-				Check: resource.ComposeTestCheckFunc(
+	rand := getAccTestRandInt(1000000, 9999999)
+	resourceId := "data.alibabacloudstack_cr_repos.default"
+	name := fmt.Sprintf("acc-cr-repos-%d", rand)
 
-					testAccCheckAlibabacloudStackDataSourceID("data.alibabacloudstack_cr_repos.default"),
-					resource.TestCheckNoResourceAttr("data.alibabacloudstack_cr_repos.default", "repos.namespace"),
-					resource.TestCheckNoResourceAttr("data.alibabacloudstack_cr_repos.default", "repos.name"),
-					resource.TestCheckNoResourceAttr("data.alibabacloudstack_cr_repos.default", "repos.repo_type"),
-					resource.TestCheckNoResourceAttr("data.alibabacloudstack_cr_repos.default", "repos.summary"),
-					resource.TestCheckResourceAttrSet("data.alibabacloudstack_cr_repos.default", "ids.#"),
-				),
-			},
-		},
-	})
+	testAccConfig := dataSourceTestAccConfigFunc(resourceId, name, dataSourceCRReposConfigDependence)
+
+	nameRegexConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"name_regex": "${alibabacloudstack_cr_repo.default.name}",
+		}),
+		fakeConfig: testAccConfig(map[string]interface{}{
+			"name_regex": "fake_*",
+		}),
+	}
+
+	namespaceConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"namespace": "${alibabacloudstack_cr_namespace.default.name}",
+		}),
+		fakeConfig: testAccConfig(map[string]interface{}{
+			"namespace": "fake-namespace",
+		}),
+	}
+
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"namespace":  "${alibabacloudstack_cr_namespace.default.name}",
+			"name_regex": "${alibabacloudstack_cr_repo.default.name}",
+		}),
+		fakeConfig: testAccConfig(map[string]interface{}{
+			"namespace":  "fake-namespace",
+			"name_regex": "fake_*",
+		}),
+	}
+
+	var existCRReposMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":                     "1",
+			"names.#":                   "1",
+			"repos.#":                   "1",
+			"repos.0.namespace":         name,
+			"repos.0.name":              name,
+			"repos.0.summary":           "OLD",
+			"repos.0.repo_type":         "PUBLIC",
+		}
+	}
+
+	var fakeCRReposMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":     "0",
+			"names.#":   "0",
+			"repos.#":   "0",
+		}
+	}
+
+	var crReposCheckInfo = dataSourceAttr{
+		resourceId:   resourceId,
+		existMapFunc: existCRReposMapFunc,
+		fakeMapFunc:  fakeCRReposMapFunc,
+	}
+	crReposCheckInfo.dataSourceTestCheck(t, rand, nameRegexConf, namespaceConf, allConf)
 }
 
-func dataSourceCRReposConfigDataSource() string {
+func dataSourceCRReposConfigDependence(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
-    default = "acc-cr-repos-%d"
+	default = "%s"
 }
 
 resource "alibabacloudstack_cr_namespace" "default" {
-    name = "${var.name}"
-    auto_create	= false
-    default_visibility = "PUBLIC"
+	name               = var.name
+	auto_create        = false
+	default_visibility = "PUBLIC"
 }
 
 resource "alibabacloudstack_cr_repo" "default" {
-    namespace = "${alibabacloudstack_cr_namespace.default.name}"
-    name = "${var.name}"
-    summary = "OLD"
-    repo_type = "PUBLIC"
-    detail  = "OLD"
+	namespace = alibabacloudstack_cr_namespace.default.name
+	name      = var.name
+	summary   = "OLD"
+	repo_type = "PUBLIC"
+	detail    = "OLD"
 }
 
-data "alibabacloudstack_cr_repos" "default" {
-  name_regex = alibabacloudstack_cr_repo.default.name
-}
-
-`, getAccTestRandInt(1000000, 9999999))
+`, name)
 }
