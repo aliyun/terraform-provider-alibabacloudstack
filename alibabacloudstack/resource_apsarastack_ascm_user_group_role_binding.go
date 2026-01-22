c95eda14d8bd4cf2ee21c22d54c4b1936fea1120
宇轩 <dyx02092379@alibaba-inc.com>
2026-01-22 16:54:32 +0800

    fix ascm user group role binding

--- alibabacloudstack/resource_apsarastack_ascm_user_group_role_binding.go
+++ alibabacloudstack/resource_apsarastack_ascm_user_group_role_binding.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/connectivity"
 	"github.com/aliyun/terraform-provider-alibabacloudstack/alibabacloudstack/errmsgs"
@@ -16,14 +17,19 @@ func resourceAlibabacloudStackAscmUserGroupRoleBinding() *schema.Resource {
 			"user_group_id": {
 				Type:     schema.TypeInt,
 				Required: true,
+				ForceNew: true,
 			},
 			"role_ids": {
 				Type:     schema.TypeSet,
 				Optional: true,
+				Computed: true,
 				Elem:     &schema.Schema{Type: schema.TypeInt},
 			},
 		},
 		DeprecationMessage: "ascm_user_group already includes corresponding functions",
+		Importer: &schema.ResourceImporter{
+			State: resourceAlibabacloudStackAscmUserGroupRoleBindingImportState,
+		},
 	}
 	setResourceFunc(resource,
 		resourceAlibabacloudStackAscmUserGroupRoleBindingCreate,
@@ -33,49 +39,63 @@ func resourceAlibabacloudStackAscmUserGroupRoleBinding() *schema.Resource {
 	return resource
 }
 
+func resourceAlibabacloudStackAscmUserGroupRoleBindingImportState(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	id := d.Id()
+
+	if strings.HasPrefix(id, "group:") {
+		id = strings.TrimPrefix(id, "group:")
+	}
+
+	userGroupId, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid user_group_id in import ID: must be integer")
+	}
+
+	d.Set("user_group_id", userGroupId)
+	d.SetId(strconv.Itoa(userGroupId))
+
+	return []*schema.ResourceData{d}, nil
+}
+
 func resourceAlibabacloudStackAscmUserGroupRoleBindingCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AlibabacloudStackClient)
 	userGroupId := d.Get("user_group_id").(int)
-	flag := false
 	var roleids []int
 	if v, ok := d.GetOk("role_ids"); ok {
 		roleids = expandIntList(v.(*schema.Set).List())
 	}
 	log.Printf("roleids is %v", roleids)
-	flag = true
-	if flag {
-		for i := range roleids {
-			request := client.NewCommonRequest("POST", "ascm", "2019-05-10", "AddRoleToUserGroup", "/ascm/auth/user/addRoleToUserGroup")
-			mergeMaps(request.QueryParams, map[string]string{
-				"ProductName":      "ascm",
-				"userGroupId":      strconv.Itoa(userGroupId),
-				"RoleId":           fmt.Sprint(roleids[i]),
-				"SecurityToken":    client.Config.SecurityToken,
-				"SignatureVersion": "1.0",
-				"SignatureMethod":  "HMAC-SHA1",
-			})
-			bresponse, err := client.ProcessCommonRequest(request)
-			log.Printf("response of raw AddRoleToUserGroup Role(%d) is : %s", roleids[i], bresponse)
 
-			if err != nil {
-				errmsg := ""
-				if bresponse != nil {
-					errmsg = errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
-				}
-				return errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "alibabacloudstack_ascm_usergroup_role_binding", "AddRoleToUserGroup", errmsgs.AlibabacloudStackSdkGoERROR, errmsg)
-			}
+	for i := range roleids {
+		request := client.NewCommonRequest("POST", "ascm", "2019-05-10", "AddRoleToUserGroup", "/ascm/auth/user/addRoleToUserGroup")
+		mergeMaps(request.QueryParams, map[string]string{
+			"ProductName":      "ascm",
+			"userGroupId":      strconv.Itoa(userGroupId),
+			"RoleId":           fmt.Sprint(roleids[i]),
+			"SecurityToken":    client.Config.SecurityToken,
+			"SignatureVersion": "1.0",
+			"SignatureMethod":  "HMAC-SHA1",
+		})
+		bresponse, err := client.ProcessCommonRequest(request)
+		log.Printf("response of raw AddRoleToUserGroup Role(%d) is : %s", roleids[i], bresponse)
 
-			addDebug("AddRoleToUserGroup", bresponse, request, request.QueryParams)
-			if bresponse.GetHttpStatus() != 200 {
-				errmsg := errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
-				return errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "alibabacloudstack_ascm_usergroup_role_binding", "AddRoleToUserGroup", errmsgs.AlibabacloudStackSdkGoERROR, errmsg)
+		if err != nil {
+			errmsg := ""
+			if bresponse != nil {
+				errmsg = errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
 			}
-			log.Printf("response of queryparams AddRoleToUserGroup is : %s", request.QueryParams)
+			return errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "alibabacloudstack_ascm_usergroup_role_binding", "AddRoleToUserGroup", errmsgs.AlibabacloudStackSdkGoERROR, errmsg)
 		}
+
+		addDebug("AddRoleToUserGroup", bresponse, request, request.QueryParams)
+		if bresponse.GetHttpStatus() != 200 {
+			errmsg := errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
+			return errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "alibabacloudstack_ascm_usergroup_role_binding", "AddRoleToUserGroup", errmsgs.AlibabacloudStackSdkGoERROR, errmsg)
+		}
+		log.Printf("response of queryparams AddRoleToUserGroup is : %s", request.QueryParams)
 	}
 
 	d.SetId(strconv.Itoa(userGroupId))
-
 	return nil
 }
 
@@ -94,9 +114,11 @@ func resourceAlibabacloudStackAscmUserGroupRoleBindingRead(d *schema.ResourceDat
 		d.SetId("")
 		return nil
 	}
-	atoi, err := strconv.Atoi(d.Id())
-	d.Set("user_group_id", atoi)
-	role_ids := []int{}
+
+	userGroupId, _ := strconv.Atoi(d.Id())
+	d.Set("user_group_id", userGroupId)
+
+	role_ids := make([]int, 0)
 	for _, role := range object.Data[0].Roles {
 		role_ids = append(role_ids, role.Id)
 	}
@@ -106,18 +128,10 @@ func resourceAlibabacloudStackAscmUserGroupRoleBindingRead(d *schema.ResourceDat
 }
 
 func resourceAlibabacloudStackAscmUserGroupRoleBindingUpdate(d *schema.ResourceData, meta interface{}) error {
-	var roleIdList []string
 	user_group_id := d.Get("user_group_id").(int)
 	client := meta.(*connectivity.AlibabacloudStackClient)
 	ascmService := AscmService{client}
 
-	if v, ok := d.GetOk("role_ids"); ok {
-		roleids := expandIntList(v.(*schema.Set).List())
-
-		for _, roleid := range roleids {
-			roleIdList = append(roleIdList, strconv.Itoa(roleid))
-		}
-	}
 	if d.HasChange("role_ids") {
 		o, n := d.GetChange("role_ids")
 		oldValue := make(map[int]struct{})
@@ -136,7 +150,7 @@ func resourceAlibabacloudStackAscmUserGroupRoleBindingUpdate(d *schema.ResourceD
 			newValue[v.(int)] = struct{}{}
 		}
 
-		for key, _ := range oldValue {
+		for key := range oldValue {
 			if _, exist := newValue[key]; !exist {
 				requestBody := map[string]interface{}{
 					"userGroupId": user_group_id,
@@ -147,7 +161,7 @@ func resourceAlibabacloudStackAscmUserGroupRoleBindingUpdate(d *schema.ResourceD
 				}
 			}
 		}
-		for key, _ := range newValue {
+		for key := range newValue {
 			if _, exist := oldValue[key]; !exist {
 				requestBody := map[string]interface{}{
 					"userGroupId": user_group_id,
