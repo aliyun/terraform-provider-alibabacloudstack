648b4ebea61291280dc611406d9e0d057e6fb51b
dwt01285077 <dwt01285077@alibaba-inc.com>
2026-04-21 14:56:28 +0800

    refactor: remove unused code and add json encoding import
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- alibabacloudstack/data_source_apsarastack_oss_clusters.go
+++ alibabacloudstack/data_source_apsarastack_oss_clusters.go
@@ -1,6 +1,7 @@
 package alibabacloudstack
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 
@@ -103,36 +104,31 @@ func dataSourceAlibabacloudStackOssClusters() *schema.Resource {
 
 func dataSourceAlibabacloudStackOssClustersRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AlibabacloudStackClient)
-	// request := client.NewCommonRequest("GET", "OneRouter", "2018-12-12", "DoApi", "")
-	// request.SetDomain(client.Config.Endpoints[connectivity.ASAPICode])
-	// request.QueryParams["AppAction"] = "GetOssEndpointList"
-	// request.QueryParams["AppName"] = "one-console-app-oss"
-	// region := client.RegionId
-	// request.QueryParams["Params"] = fmt.Sprintf("{\"region\":\"%s\", \"params\":{\"region\":\"%s\"}}", region, region)
-	// bresponse, err := client.ProcessCommonRequest(request)
-	// addDebug("GetOssEndpointList", bresponse, request, request.QueryParams)
-	// if err != nil {
-	// 	if bresponse == nil {
-	// 		return errmsgs.WrapErrorf(err, "Process Common Request Failed")
-	// 	}
-	// 	errmsg := errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
-	// 	return errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "GetOssEndpointList", errmsgs.AlibabacloudStackOssGoSdk, errmsg)
-	// }
-	// result := make(map[string]interface{})
-	// _ = json.Unmarshal(bresponse.GetHttpContentBytes(), &result)
-	// data, ok := result["Data"]
-	// if !ok || len(data.([]interface{})) == 0 {
-	// 	return errmsgs.Error(fmt.Sprintf("GetOssEndpointList Failed! region: %s \n %#v", client.RegionId, bresponse.GetHttpContentString()))
-	// }
-	ossService := OssService{client}
-	data, err := ossService.GetOssEndpointListForIot()
+	request := client.NewCommonRequest("GET", "OneRouter", "2018-12-12", "DoApi", "")
+	request.SetDomain(client.Config.Endpoints[connectivity.ASAPICode])
+	request.QueryParams["AppAction"] = "GetOssEndpointList"
+	request.QueryParams["AppName"] = "one-console-app-oss"
+	region := client.RegionId
+	request.QueryParams["Params"] = fmt.Sprintf("{\"region\":\"%s\", \"params\":{\"region\":\"%s\"}}", region, region)
+	bresponse, err := client.ProcessCommonRequest(request)
+	addDebug("GetOssEndpointList", bresponse, request, request.QueryParams)
 	if err != nil {
-		return errmsgs.WrapError(err)
+		if bresponse == nil {
+			return errmsgs.WrapErrorf(err, "Process Common Request Failed")
+		}
+		errmsg := errmsgs.GetBaseResponseErrorMessage(bresponse.BaseResponse)
+		return errmsgs.WrapErrorf(err, errmsgs.RequestV1ErrorMsg, "GetOssEndpointList", errmsgs.AlibabacloudStackOssGoSdk, errmsg)
+	}
+	result := make(map[string]interface{})
+	_ = json.Unmarshal(bresponse.GetHttpContentBytes(), &result)
+	data, ok := result["Data"]
+	if !ok || len(data.([]interface{})) == 0 {
+		return errmsgs.Error(fmt.Sprintf("GetOssEndpointList Failed! region: %s \n %#v", client.RegionId, bresponse.GetHttpContentString()))
 	}
 	idsMap := getIdsStringFilter(d)
 	ids := make([]string, 0)
 	clusters := make([]map[string]interface{}, 0)
-	for _, v := range data {
+	for _, v := range data.([]interface{}) {
 		object := v.(map[string]interface{})
 		if len(idsMap) > 0 {
 			if _, ok := idsMap[object["cluster"].(string)]; !ok {
