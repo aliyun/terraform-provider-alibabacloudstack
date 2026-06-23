5ab51a251965c24d4a9c4eb0377f932a56d84e26
jingyu <jingyu.wy@alibaba-inc.com>
2026-06-23 14:57:04 +0800

    fix cms endpoint template

--- alibabacloudstack/connectivity/endpoint.go
+++ alibabacloudstack/connectivity/endpoint.go
@@ -257,8 +257,8 @@ var PopEndpoints = map[ServiceCode]PopEndpoint{
 		"ram.{{.domain}}",
 	},
 	CMSCode: PopEndpoint{
-		"metrics.open.{{.domain}}",
-		"metrics.open.{{.region}}.{{.domain}}",
+		"metrics-vpc.open.{{.domain}}",
+		"metrics-vpc.open.{{.region}}.{{.domain}}",
 	},
 	HitsdbCode: PopEndpoint{
 		"hitsdb.{{.domain}}",
